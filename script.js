7b10b5a9eb7ced4d1e12bc69a85cd11034619ab0
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-12-22 13:12:47 -0800

    Establish classes and primary function

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // Build a binary search tree
+
+class Node {
+    constructor(value){
+    this.value = value;
+    this.previous = null;
+    this.next = null;
+    }
+}
+
+class Tree {
+    constructor(array){
+        this.array = array;
+        this.root
+    }
+}
+
+function buildTree(array){
+    // This function should take an array of numbers and turns it into a balanced binary tree full of Node objects appropriately placed.
+    // The first step is to sort the array and remove any duplicates
+    
+}
