a04c7eeea83ca6af3435ec496e75445089241937
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-12-28 15:09:09 -0500

    Refactor first version but catch 3+ duplicate bug

--- script.js
+++ script.js
@@ -20,29 +20,31 @@ function buildTree(array) {
   // The first step is to sort the array and remove any duplicates.
   // This function should return the level-0 root node
 
-  let fullArray = array;
   let temp;
   let finalArray = [];
-  while (fullArray.length > 0) {
-    temp = fullArray.splice(0, 1);
-    temp = Number(temp);
-    for (let i = 0; i < fullArray.length; i++) {
-      if (temp == fullArray[i]) {
-        fullArray.splice(i, 1);
+  while (array.length > 0) {
+    temp = Number(array.splice(0, 1));
+    for (let i = 0; i < array.length; i++) {
+      if (temp === array[i]) {
+        array.splice(i, 1);
       }
     }
     finalArray.push(temp);
   }
-  return finalArray;
+  array = finalArray;
+  return array;
 }
 
 let testArray1 = [6, 2];
 let testArray2 = [6, 3, 6];
 let testArray3 = [6, 3, 9, 2];
 let testArray4 = [6, 3, 9, 2, 3, 9, 9];
-let testArray5 = [10, 2, 3, 4, 5, 5, 6, 1, 7];
+let testArray5 = [10, 2, 3, 4, 5, 5, 6, 1, 7, 1, 1, 1, 1];
+
 
 // console.log(buildTree(testArray1));
 // console.log(buildTree(testArray2));
 console.log(buildTree(testArray5));
 console.log(buildTree(testArray2));
+
+// We still haven't solved the issue of finding and fixing an element that occurs more than twice
