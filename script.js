0776b10004afc9984075cac974dd3251771f355a
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-01-02 09:39:16 -0500

    Create binary tree for array of length 3

--- script.js
+++ script.js
@@ -61,37 +61,40 @@ function buildTree(array) {
   console.log(`firstHalf: ${firstHalf}`);
   let lastHalf = formattedArray.slice(middleNodeIndex + 1);
   console.log(`lastHalf: ${lastHalf}`);
-  
 
+  // I think our base case needs to be length = 3. Then we can call recursively for longer arrays, and assign previous and next for the 3 nodes available
+  // If the array length is less than three, then one of the nodes is a ?leaf node? (A node with only one child)
+  if (formattedArray.length == 3) {
+    console.log(`found 3 length array`);
 
-// I think our base case needs to be length = 3. Then we can call recursively for longer arrays, and assign previous and next for the 3 nodes available
-// If the array length is less than three, then one of the nodes is a ?leaf node? (A node with only one child)
-if (array.length ==3){
-    rootNode.previous = new Node(formattedArray[0]);
-    rootNode.previous.next = rootNode;
+    let previous = new Node(formattedArray[0]);
+    let next = new Node(formattedArray[2]);
 
-    rootNode.next = new Node(formattedArray[2]);
-    rootNode.next.previous = rootNode;
-}
+    rootNode.previous = previous;
+    rootNode.next = next;
 
-if (array.length == 0){
+    previous.next = rootNode;
+    next.previous = rootNode;
+    console.log(`next.previous: ${next.previous}`);
+    console.log(next.previous);
+    console.log(next);
+  }
+
+  if (formattedArray.length == 0) {
     console.log(`empty array found`);
     return null;
-}
-else if (array.length = 1){
+  } else if ((formattedArray.length = 1)) {
     console.log(`found array length 1`);
     // return new Node(array[0]);
-}
-let firstSideRoot = buildTree(firstHalf);
-let lastSideRoot = buildTree(lastHalf);
+  }
+//   let firstSideRoot = buildTree(firstHalf);
+//   let lastSideRoot = buildTree(lastHalf);
 
-console.log(`firstSideRoot: ${firstSideRoot} `);
-console.log(`lastSideRoot: ${lastSideRoot}`);
+//   console.log(`firstSideRoot: ${firstSideRoot} `);
+//   console.log(`lastSideRoot: ${lastSideRoot}`);
 
-
-    rootNode.previous = buildTree(firstHalf);
-    rootNode.next = buildTree(lastHalf);
-
+//   rootNode.previous = buildTree(firstHalf);
+//   rootNode.next = buildTree(lastHalf);
 
   return rootNode;
 }
@@ -99,6 +102,6 @@ console.log(`lastSideRoot: ${lastSideRoot}`);
 let testArray1 = [6, 2, 22, 12, 54, 22];
 let testArray2 = [12, 3, 54, 109, 54, 3, 423];
 let testArray3 = [6, 2];
-let testArray4 = [6, 2, 3]
+let testArray4 = [6, 2, 3];
 
 console.log(buildTree(testArray4));
