647cbc2f555e778fa38f1dc7c1e3456a0c1e18ac
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-01-02 18:11:30 -0500

    Complete refactoring

--- script.js
+++ script.js
@@ -49,50 +49,51 @@ function formatArray(array) {
 
 function buildTree(array) {
   let formattedArray = formatArray(array);
-  let middleNodeIndex = Math.floor(formattedArray.length / 2);
-  console.log(`middleNodeIndex: ${middleNodeIndex}`);
-  let rootNodeValue = formattedArray[middleNodeIndex];
-  console.log(`rootNodeValue: ${rootNodeValue}`);
-  let rootNode = new Node(rootNodeValue);
-
-  // Now we gotta establish 2 arrays for elements before and after the root node
-  console.log(`formattedArray: ${formattedArray}`);
-  let firstHalf = formattedArray.slice(0, middleNodeIndex);
-  console.log(`firstHalf: ${firstHalf}`);
-  let lastHalf = formattedArray.slice(middleNodeIndex + 1);
-  console.log(`lastHalf: ${lastHalf}`);
-
-  // I think our base case needs to be length = 3. Then we can call recursively for longer arrays, and assign previous and next for the 3 nodes available
-  // If the array length is less than three, then one of the nodes is a ?leaf node? (A node with only one child)
-  if (formattedArray.length == 3) {
-    console.log(`found 3 length array`);
-
-    let previous = new Node(formattedArray[0]);
-    let next = new Node(formattedArray[2]);
-
-    rootNode.previous = previous;
-    rootNode.next = next;
-
-    previous.next = rootNode;
-    next.previous = rootNode;
-    console.log(`next.previous: ${next.previous}`);
-    console.log(next.previous);
-    console.log(next);
-  }
-
+let rootNode;
   if (formattedArray.length == 0) {
-    console.log(`empty array found`);
     return null;
-  } else if ((formattedArray.length == 1)) {
-    console.log(`found array length 1`);
-    // return new Node(array[0]);
-  } else if (formattedArray.length > 3){
-    // This should happen higher up
+  } else if (formattedArray.length === 1) {
+    rootNode = new Node(formattedArray[0]);
+    // return rootNode;
+  } else if (formattedArray.length === 2) {
+    rootNode = new Node(formattedArray[0]);
+    let nextNode = new Node(formattedArray[1]);
+    rootNode.next = nextNode;
+    nextNode.previous = rootNode;
+  } else if (formattedArray.length === 3) {
+    rootNode = new Node(formattedArray[1]);
+    let previousNode = new Node(formattedArray[0]);
+    let nextNode = new Node(formattedArray[2]);
+    previousNode.next = rootNode;
+    rootNode.previous = previousNode;
+    rootNode.next = nextNode;
+    nextNode.previous = rootNode;
+    // return rootNode;
+  } else {
+    // Here we need to use recursion
+
+    let middleNodeIndex = Math.floor(formattedArray.length / 2);
+    console.log(`middleNodeIndex: ${middleNodeIndex}`);
+    let rootNodeValue = formattedArray[middleNodeIndex];
+    console.log(`rootNodeValue: ${rootNodeValue}`);
+
+    rootNode = new Node(rootNodeValue);
+
+    // Now we gotta establish 2 arrays for elements before and after the root node
+    console.log(`formattedArray: ${formattedArray}`);
+    let firstHalf = formattedArray.slice(0, middleNodeIndex);
+    console.log(`firstHalf: ${firstHalf}`);
+    let lastHalf = formattedArray.slice(middleNodeIndex + 1);
+    console.log(`lastHalf: ${lastHalf}`);
+
+    let previousNode = buildTree(firstHalf);
+    let nextNode = buildTree(lastHalf);
+
+    previousNode.next = rootNode;
+    rootNode.previous = previousNode;
+    rootNode.next = nextNode;
+    nextNode.previous = rootNode;
   }
-// ***********************************************************************************************
-// If there are 3 nodes, create a node for each and link. If there are more than three, create root node, and link next and previous to a recursive call of buildTree
-// If there's 2 nodes, pick either the smaller or bigger one to be the root node, and link them. Shouldn't matter which as long as we're consistent
-// ***********************************************************************************************
 
   return rootNode;
 }
@@ -102,4 +103,4 @@ let testArray2 = [12, 3, 54, 109, 54, 3, 423];
 let testArray3 = [6, 2];
 let testArray4 = [6, 2, 3];
 
-console.log(buildTree(testArray4));
+console.log(buildTree(testArray1));
