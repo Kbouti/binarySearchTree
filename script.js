beaaf9b5e37b74e4919460c50e4ca5aaa18af9de
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-01-04 13:33:15 -0800

    Fix prettyPrint function

--- script.js
+++ script.js
@@ -8,6 +8,12 @@ class Node {
     this.left = null;
     this.right = null;
   }
+  isLeafNode(){
+    if ((this.right == null) && (this.left == null)){
+        return true;
+    }
+    return false;
+  }
 }
 
 class Tree {
@@ -47,54 +53,42 @@ function formatArray(array) {
   return newArray;
 }
 
-const prettyPrint = (node, prefix = "", isLeft = true) => {
-  if (node === null) {
-    return;
-  }
-  if (node.right !== null) {
-    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
-  }
-  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
-  if (node.left !== null) {
-    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
-  }
-};
 
 function buildTree(array) {
   let formattedArray = formatArray(array);
   let rootNode;
   if (formattedArray.length == 0) {
-    console.log(`array length == 0`);
+    // console.log(`array length == 0`);
     return null;
   } else if (formattedArray.length === 1) {
-    console.log(`array length == 1`);
+    // console.log(`array length == 1`);
     rootNode = new Node(formattedArray[0]);
   } else if (formattedArray.length === 2) {
-    console.log(`array length == 2`);
+    // console.log(`array length == 2`);
     rootNode = new Node(formattedArray[0]);
     let nextNode = new Node(formattedArray[1]);
     rootNode.right = nextNode;
-    console.log(rootNode.right);
+    // console.log(rootNode.right);
   } else if (formattedArray.length === 3) {
-    console.log(`array length == 3`);
-    console.log(`rootNode value: ${formattedArray[1]}`);
+    // console.log(`array length == 3`);
+    // console.log(`rootNode value: ${formattedArray[1]}`);
     rootNode = new Node(formattedArray[1]);
     let previousNode = new Node(formattedArray[0]);
     let nextNode = new Node(formattedArray[2]);
     rootNode.left = previousNode;
     rootNode.right = nextNode;
   } else {
-    console.log(`array length > 3`);
+    // console.log(`array length > 3`);
     let middleNodeIndex = Math.floor(formattedArray.length / 2);
-    console.log(`middleNodeIndex: ${middleNodeIndex}`);
+    // console.log(`middleNodeIndex: ${middleNodeIndex}`);
     let rootNodeValue = formattedArray[middleNodeIndex];
-    console.log(`rootNodeValue: ${rootNodeValue}`);
+    // console.log(`rootNodeValue: ${rootNodeValue}`);
     rootNode = new Node(rootNodeValue);
-    console.log(`formattedArray: ${formattedArray}`);
+    // console.log(`formattedArray: ${formattedArray}`);
     let firstHalf = formattedArray.slice(0, middleNodeIndex);
-    console.log(`firstHalf: ${firstHalf}`);
+    // console.log(`firstHalf: ${firstHalf}`);
     let lastHalf = formattedArray.slice(middleNodeIndex + 1);
-    console.log(`lastHalf: ${lastHalf}`);
+    // console.log(`lastHalf: ${lastHalf}`);
     let previousNode = buildTree(firstHalf);
     let nextNode = buildTree(lastHalf);
     rootNode.left = previousNode;
@@ -102,6 +96,7 @@ function buildTree(array) {
   }
   return rootNode;
 }
+
 let testArray1 = [6];
 let testArray2 = [6, 2];
 let testArray3 = [6, 2, 3];
@@ -119,7 +114,25 @@ const testArray7 = [7, 6, 5, 4, 3, 2, 1];
 // prettyPrint(testNode);
 
 
-let testTree = new Tree(testArray4);
-console.log(testTree);
+let testTree = new Tree(testArray5);
+// console.log(testTree);
 
-prettyPrint(testTree.root, prefix = "", isLeft = true)
+
+
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  };
+  
+
+// prettyPrint(testTree.root, prefix = "", isLeft = true);
+prettyPrint(testTree.root, prefix = "", isLeft = true);
